41790f26a4cc38b30b634caa19e0aeab87f795cf
Kirill <sgaska@bk.ru>
2025-10-26 20:27:13 +0300

    Добавление маршрутизированного вертикального возврата

--- lib/features/payments/screens/settings_screen.dart
+++ lib/features/payments/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -11,7 +12,7 @@ class SettingsScreen extends StatelessWidget {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           tooltip: 'На главный экран',
-          onPressed: () => Navigator.pop(context),
+          onPressed: () => context.pop(),
         ),
       ),
       body: const Center(
